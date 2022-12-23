8be07f925aa6221e0bb9a5f0beb366f7cfc18464
JoshTrono <59839694+JoshTrono@users.noreply.github.com>
2022-12-23 00:51:32 -0500

    did task 1

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,14 +1,37 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+  const { budget, dispatch } = useContext(AppContext);
+  const [newBudget, setbudget] = useState(budget);
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  // took the dispatch to submit a appreducer that take the set_budget input
+  // with the new budget which is the newbudget variable origionally
+  // the budget when initiallized but changed whenever the screen changes.
+
+  const submitEvent = () => {
+    dispatch({
+      type: "SET_BUDGET",
+      payload: newBudget,
+    });
+  };
+
+  const twoCalls = (event) => {
+    setbudget(event.target.value);
+    submitEvent();
+  };
+  return (
+    <div className="alert alert-secondary">
+      <span>
+        Budget: £
+        <input
+          type="number"
+          value={newBudget}
+          onChange={(e) => twoCalls(e)}
+        ></input>
+      </span>
+    </div>
+  );
 };
 
 export default Budget;
